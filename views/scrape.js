863b73a07d1057e168a8a3e6f32f925b002218b3
Eirikur Nilsson <eirikur@nilsson.is>
2012-10-02 21:22:44 +0000

    Send user and provider info to scrape.
    Also expose env to templates.
    Implemented basic api with CORS in scrape.js.
    Basic error handling in scrape.js.

--- views/scrape.js
+++ views/scrape.js
@@ -1,3 +1,50 @@
 (function() {
-<%- indent(scraper, 2) %>
+  var passUrl;
+
+  function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+      // XHR for Chrome/Safari/Firefox.
+      xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+      // XDomainRequest for IE.
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+    } else {
+      // CORS not supported.
+      xhr = null;
+    }
+    return xhr;
+  }
+
+  var createPass = function(data) {
+    var xhr = createCORSRequest('POST', passUrl);
+
+    if (!xhr) {
+      alert("Browser doesn't support this bookmarklet. (CORS)");
+      return;
+    }
+
+    xhr.onload = function() {
+      alert(xhr.responseText);
+    };
+
+    xhr.onerror = function() {
+      alert("Could not create passlet.");
+    }
+
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+  };
+
+<% if (!user) { %>
+  alert('Could not authenticate with passlet service.');
+<% } else if (!provider) { %>
+  alert('Sorry, this website is not supported.');
+<% } else { %>
+  passUrl = "<%- env.PASSLET_BASE_URL %>/pass/<%- provider.name %>";
+
+<%- indent(provider.scraper, 2) %>
+<% } %>
+
 })();
